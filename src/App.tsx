7dc081b78a924d08cbd1e716509398b878e970ed
lvd. <nbikiti@gmail.com>
2025-10-18 02:28:45 +0400

    Comment emulator

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/modules/Header.tsx"
 import Body from "./components/modules/Body.tsx"
 import LoadingScreen from "./components/modules/LoadingScreen.tsx"
 import {useGame} from "./components/GameContext.tsx";
-import './components/telegramEmulator.ts'
+// import './components/telegramEmulator.ts'
 
 function App() {
     const gameContext = useGame()
